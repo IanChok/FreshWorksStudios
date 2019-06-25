0314bbd4019204cf70cab6f92e72d3af0bcaf15b
IanChok <ichok87@outlook.com>
2019-06-25 08:10:56 -0700

    somewhat working production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB7a3Ez8JOtXeP5CjYiehAJ2s87G3Yn9YE',
+    authDomain: 'duck-counter.firebaseapp.com',
+    databaseURL: 'https://duck-counter.firebaseio.com',
+    projectId: 'duck-counter',
+    storageBucket: 'duck-counter.appspot.com',
+    messagingSenderId: '923812696229',
+    appId: '1:923812696229:web:3bcbc763d67e9db4'
+  }
 };
